5572d68cc22d3db8cfc2c5d5a1ed42412f5124fa
Daniel Yule <daniel.yule@gmail.com>
2016-08-16 15:28:57 -0300

    Created documentation

---
+++ implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["optra"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='optra/struct.InsertOperation.html' title='optra::InsertOperation'>InsertOperation</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='optra/struct.DeleteOperation.html' title='optra::DeleteOperation'>DeleteOperation</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='struct' href='optra/struct.State.html' title='optra::State'>State</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
