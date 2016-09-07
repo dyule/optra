f0ba5cf77fcf6b2e93f0f315c2cb7ef64b9f1812
Daniel Yule <daniel.yule@gmail.com>
2016-09-07 20:29:47 -0300

    Updated docs to v0.2

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["optra"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='optra/struct.InsertOperation.html' title='optra::InsertOperation'>InsertOperation</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='optra/struct.DeleteOperation.html' title='optra::DeleteOperation'>DeleteOperation</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='optra/struct.State.html' title='optra::State'>State</a>",];
+implementors["optra"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='optra/struct.InsertOperation.html' title='optra::InsertOperation'>InsertOperation</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='optra/struct.DeleteOperation.html' title='optra::DeleteOperation'>DeleteOperation</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
